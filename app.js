277898d40c803ee87bb72d4e53d564d2cf6c0beb
K <k@Kui-MacBook-Pro.local>
2015-11-11 21:13:00 +0900

    electron multi window

---
+++ app.js
@@ -0,0 +1,28 @@
+var app = require('app')
+var BrowserWindow = require('browser-window')
+var ipc = require('ipc')
+
+app.on('ready', function() {
+  var mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+  mainWindow.loadUrl('file://' + __dirname + '/main.html')
+  mainWindow.openDevTools()
+
+  var prefsWindow = new BrowserWindow({
+    width: 400,
+    heigth: 400,
+    show: false
+  })
+  prefsWindow.loadUrl('file://' + __dirname + '/prefs.html')
+
+  // send
+  ipc.on('toggle-prefs', function(){
+    if (prefsWindow.isVisible())
+      prefsWindow.hide()
+    else
+      prefsWindow.show()
+  })
+
+})
